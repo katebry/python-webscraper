ba9dd49c02738ae29451d4f17ce2763e384bf478
Kate Bryan <katebryan31@gmail.com>
2021-05-08 13:00:36 +0100

    feat: added company element and skipped None types

--- webscraper.py
+++ webscraper.py
@@ -11,6 +11,13 @@ job_elems = results.find_all('div', class_='jobsearch-SerpJobCard')
 
 for job_elem in job_elems:
     title_elem = job_elem.find('a', class_='jobtitle')
+    company_elem = job_elem.find('span', class_='company')
     salary_elem = job_elem.find('span', class_='salaryText')
     location_elem = job_elem.find('span', class_='location')
-    print(title_elem, salary_elem, location_elem, end='\n'*4)
+    if None in (title_elem, company_elem, salary_elem, location_elem):
+        continue
+    print(title_elem.text.strip())
+    print(company_elem.text.strip())
+    print(salary_elem.text.strip())
+    print(location_elem.text.strip())
+    print()
