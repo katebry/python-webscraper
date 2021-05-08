2ca980635e9f463cdd6519b2db94b830d8a474f0
Kate Bryan <katebryan31@gmail.com>
2021-05-08 12:12:56 +0100

    fix: updated url and find

--- webscraper.py
+++ webscraper.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.monster.com/jobs/search/?q=Process+Engineer&where=UK'
+URL = 'https://uk.indeed.com/jobs?q=process+engineer&l=Manchester,+Greater+Manchester&from=mobRdr&utm_source=%2Fm%2F&utm_medium=redir&utm_campaign=dt'
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
+results = soup.find(id='resultsCol')
 
-print(soup)
+print(results)
