651883e7128d95de6eacc7eb1706ae1c9683c346
Kate Bryan <katebryan31@gmail.com>
2021-05-08 11:49:38 +0100

    feat: added beautifulSoup

--- webscraper.py
+++ webscraper.py
@@ -1,4 +1,9 @@
 import requests
+from bs4 import BeautifulSoup
 
-URL = 'https://www.monster.com/jobs/search/?q=Process-Engineer&where=UK'
+URL = 'https://www.monster.com/jobs/search/?q=Process+Engineer&where=UK'
 page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+print(soup)
